c590bd8fcbad4e7d25ed992d033efc0cc1502ecc
rodriguezz4060 <59284261+rodriguezz4060@users.noreply.github.com>
2025-06-01 09:47:47 +0300

    Update guide-hero-talents-editor.tsx

--- src/components/shared/class-guides/editor/guide-hero-talents-editor.tsx
+++ src/components/shared/class-guides/editor/guide-hero-talents-editor.tsx
@@ -2,30 +2,14 @@
 
 import { useState } from 'react';
 import { MDTabContentEditor } from './components/md-tab-content-editor';
-import { TabsEditor } from './tabs-editor';
-import { HeroTalentsProps } from '@root/@types/prisma';
 
-interface GuideHeroTalentsProps {
-  heroTalents: HeroTalentsProps;
-  guideId: number;
-}
-
-export const GuideHeroTalentsEditor = ({
-  guideId,
-  heroTalents,
-}: GuideHeroTalentsProps) => {
+export const GuideHeroTalentsEditor = () => {
   const [markdownContent, setMarkdownContent] = useState('');
   return (
     <div>
       <MDTabContentEditor
         content={markdownContent}
         onContentChange={setMarkdownContent}
-      />
-
-      <TabsEditor
-        initialTabs={heroTalents.tabs}
-        defaultTab={''}
-        guideId={guideId}
       />
     </div>
   );
